4f78090baa9e2000b744763a5473a15933e9da2d
ruisousamoreira <ruisousamoreira@gmail.com>
2022-11-28 13:26:04 +0000

    fiz on tooltip for multiple ids.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,16 @@
 // When the user clicks on <div>, open the popup
-function displayTooltip(text, media) {
-    var popup = document.getElementById("myTooltip");
-    // popup.innerHTML = '<video controls width=\"480\" height=\"225\" style="border-radius: 7px; margin-bottom: 10px; border-color: white; border-style: solid; border-width: 2px;"> <source src=\"assets/videos/dave.mp4\" type=\"video/mp4\"></video>';
+function displayTooltip(id, text, media) {
+    var popup = document.getElementById(id);
+
     if (media) {
         popup.innerHTML = media;
         popup.append(document.createElement('br'));
     }
+
     textDiv = document.createElement('div');
     textDiv.innerHTML = text;
     textDiv.style.fontWeight = 'bold';
     popup.append(textDiv);
+
     popup.classList.toggle("show");
 }
